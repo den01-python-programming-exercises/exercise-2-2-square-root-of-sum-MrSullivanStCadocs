31ca9fc01d949d89ba1f5fa9922046e0489aee0a
MrSullivan <>
2021-04-19 22:11:00 +0000

    imported np, variable interger inputs and sum of. sqare root of answer and print command.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
+    import numpy as np
+    number1 = int(input("Write your first number: "))
+    number2 = int(input("Write your second number: "))
+
+    number3 = number1 + number2
+
+    squareRoot = np.sqrt(number3)
+
+    print(squareRoot)
 
 if __name__ == '__main__':
     main()
